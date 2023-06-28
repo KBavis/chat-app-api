b322a8cf76c3d459cc50c76b9273a065f90c11af
Kellen Bavis <kellenbavis10@gmail.com>
2023-06-27 23:53:48 -0400

    Initalization of Repository

---
+++ src/main/java/com/real/time/chatapp/UserModelAssembler.java
@@ -0,0 +1,21 @@
+package com.real.time.chatapp;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserModelAssembler implements RepresentationModelAssembler<User, EntityModel<User>>{
+	
+	@Override
+	public EntityModel<User> toModel(User user){
+		return EntityModel.of(user,
+			linkTo(methodOn(UserController.class).one(user.getId())).withSelfRel(),
+			linkTo(methodOn(UserController.class).all()).withRel("/users"));
+	}
+}
+
