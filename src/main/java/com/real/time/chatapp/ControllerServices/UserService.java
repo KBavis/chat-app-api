36ab56b11af9ff4be46a06fb0a05002cd3572601
Kellen Bavis <kellenbavis10@gmail.com>
2023-08-15 21:05:43 -0400

    Finalization of inital impelemntation for User Service Unit Tests. TODO: Add Unit Tests to ensure that we can update a User or Delete A User as an Admin User rather than being that explicit User

--- src/main/java/com/real/time/chatapp/ControllerServices/UserService.java
+++ src/main/java/com/real/time/chatapp/ControllerServices/UserService.java
@@ -21,10 +21,10 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-	
+
 	private final UserRepository userRepository;
 	private final MessageRepository messageRepository;
-	
+
 	/**
 	 * Fetch All Users
 	 * 
@@ -33,9 +33,9 @@ public class UserService {
 	public List<User> getAllUsers() {
 		return userRepository.findAll();
 	}
-	
+
 	/**
-	 * Fetch A User By ID 
+	 * Fetch A User By ID
 	 * 
 	 * @param id
 	 * @return
@@ -43,18 +43,18 @@ public class UserService {
 	public User getUserById(Long id) {
 		return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
 	}
-	
+
 	/**
 	 * Fetch A User By Full Name
 	 * 
 	 * @param name
 	 * @return
 	 */
-	public List<User> searchUserByName(String name){
+	public List<User> searchUserByName(String name) {
 		String[] firstAndLast = name.split(" ");
 		return userRepository.searchUsersByName(firstAndLast[0], firstAndLast[1]);
 	}
-	
+
 	/**
 	 * Fetch A User By Username
 	 * 
@@ -64,21 +64,22 @@ public class UserService {
 	public List<User> searchUserByUsername(String userName) {
 		return userRepository.searchUsersByUserName(userName);
 	}
-	
+
 	/**
-	 * Update a User 
+	 * Update a User
 	 * 
 	 * @param id
 	 * @param userDTO
 	 * @return
 	 */
-	public User updateUser(Long id, UserDTO userDTO){
+	public User updateUser(Long id, UserDTO userDTO) {
 		User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
-		
-		if(!validateUser(user)) {
+
+		System.out.println("Validate User: " + validateUser(user));
+		if (!validateUser(user)) {
 			throw new UnauthorizedException(user);
 		}
-		
+
 		user.setFirstName(userDTO.getFirstName());
 		user.setLastName(userDTO.getLastName());
 		user.setPassword(userDTO.getPassword());
@@ -86,52 +87,60 @@ public class UserService {
 		user.setUserName(userDTO.getUsername());
 		return userRepository.save(user);
 	}
-	
+
 	/**
 	 * Delete a User
 	 * 
 	 * @param id
 	 */
-	public void deleteUser(Long id){
+	public void deleteUser(Long id) {
 		User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
-		
-		if(!validateUser(user)) {
+
+		if (!validateUser(user)) {
 			throw new UnauthorizedException(user);
 		}
-		
+
 		deleteMessageAndRemoveRecipients(user.getSentMessages());
 		userRepository.deleteById(id);
 	}
-	
+
 	/**
-	 * Helper Function to Delete a Message and Remove Recipients 
+	 * Helper Function to Delete a Message and Remove Recipients
 	 * 
 	 * @param list
 	 */
-	 public void deleteMessageAndRemoveRecipients(List<Message> list) {
-		if(list != null) {
+	public void deleteMessageAndRemoveRecipients(List<Message> list) {
+		if (list != null) {
 			for (Message msg : list) {
 				Set<User> recipients = msg.getRecipients();
 				for (User current : recipients) {
 					current.getRecievedMessages().remove(msg);
 				}
 				messageRepository.deleteById(msg.getMessage_id());
-			}	
+			}
 		}
 	}
-	
-	
+
 	/**
 	 * Helper Function To Validate A User
+	 * TODO: Update Below Functionality to Check if AUTHENTICATED USER is Admin, Not User Being Passed In
+	 * 
 	 * @param user
 	 * @return
 	 */
 	public boolean validateUser(User user) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String authenticatedUsername = authentication.getName().trim();
-		if(user.getRole() != Role.ADMIN && !user.getUsername().trim().equals(authenticatedUsername)) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String authenticatedUsername = authentication.getName().trim();
+
+		if (user.getRole() != Role.ADMIN && !user.getUsername().trim().equals(authenticatedUsername)) {
 			return false;
 		}
+
+		// Get Authenticated User
+//        User authUser = userRepository.findByUserName(authenticatedUsername).orElseThrow(() -> new UserNotFoundException(authenticatedUsername));
+//		if(authUser.getRole() != Role.ADMIN && !user.getUsername().trim().equals(authenticatedUsername)) {
+//			return false;
+//		}
 		return true;
 	}
 }
