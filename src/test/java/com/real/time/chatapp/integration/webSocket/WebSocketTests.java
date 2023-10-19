39410bf6563ab0032753e93ea18b3aca932b6cd9
Kellen Bavis <kellenbavis10@gmail.com>
2023-10-18 20:49:06 -0400

    Updated JsonKafkaConsumer to utilize  rather than . Generated client to connect/subscribe to ensure WebSocket functionality is working. TODO: Consider Restricting Access To WebSockets based on Users within Conversation

--- src/test/java/com/real/time/chatapp/integration/webSocket/WebSocketTests.java
+++ src/test/java/com/real/time/chatapp/integration/webSocket/WebSocketTests.java
@@ -12,13 +12,14 @@ import java.util.concurrent.TimeoutException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
@@ -28,6 +29,10 @@ import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 import com.real.time.chatapp.DTO.MessageDTO;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {
+    "spring.security.enabled=false"
+})
 public class WebSocketTests {
 	private String URL;
 	
@@ -36,21 +41,41 @@ public class WebSocketTests {
     @BeforeEach
     public void setup() {
         completableFuture = new CompletableFuture<>();
-        URL = "ws://localhost:8080/ws";
+        URL = "ws://localhost:8080/connect";
     }
     
+//    @Test
+//    @SuppressWarnings("deprecation")
+//    public void testSendMessage() throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException {
+//    	WebSocketStompClient stompClient = new WebSocketStompClient(new SockJsClient(createTransportClient()));
+//    	stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+//    	
+//        
+//		StompSession stompSession = stompClient.connect(URL, new StompSessionHandlerAdapter() {
+//        }).get(1, SECONDS);
+//		
+////		 stompSession.subscribe("/topic/conversation/1", new SendMessageStompFrameHandler());
+//    }
+    
     @Test
-    @WithMockUser
     @SuppressWarnings("deprecation")
     public void testSendMessage() throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException {
-    	WebSocketStompClient stompClient = new WebSocketStompClient(new SockJsClient(createTransportClient()));
-    	stompClient.setMessageConverter(new MappingJackson2MessageConverter());
-    	
-        
-		StompSession stompSession = stompClient.connect(URL, new StompSessionHandlerAdapter() {
+        WebSocketStompClient stompClient = new WebSocketStompClient(new SockJsClient(createTransportClient()));
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSession stompSession = stompClient.connect(URL, new StompSessionHandlerAdapter() {
+            @Override
+            public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+                // Once connected, send a MessageDTO
+                MessageDTO message = new MessageDTO();
+                // Set properties of message
+
+                session.send("/app/sendMessage", message); // Assuming this is the destination where you handle incoming messages
+            }
         }).get(1, SECONDS);
-		
-		 stompSession.subscribe("/topic/conversation/1", new SendMessageStompFrameHandler());
+
+        // Wait for the message to be received
+        completableFuture.get(5, SECONDS);
     }
     
     private List<Transport> createTransportClient() {
@@ -59,21 +84,21 @@ public class WebSocketTests {
         return transports;
     }
     
-    private class SendMessageStompFrameHandler implements StompFrameHandler {
-        @Override
-        public Type getPayloadType(StompHeaders stompHeaders) {
-            return MessageDTO.class;
-        }
-
-        @Override
-        public void handleFrame(StompHeaders stompHeaders, Object payload) {
-            if (payload instanceof MessageDTO) {
-                completableFuture.complete((MessageDTO) payload);
-            } else {
-                completableFuture.completeExceptionally(new IllegalStateException("Unexpected payload type: " + payload.getClass()));
-            }
-        }
-    }
+//    private class SendMessageStompFrameHandler implements StompFrameHandler {
+//        @Override
+//        public Type getPayloadType(StompHeaders stompHeaders) {
+//            return MessageDTO.class;
+//        }
+//
+//        @Override
+//        public void handleFrame(StompHeaders stompHeaders, Object payload) {
+//            if (payload instanceof MessageDTO) {
+//                completableFuture.complete((MessageDTO) payload);
+//            } else {
+//                completableFuture.completeExceptionally(new IllegalStateException("Unexpected payload type: " + payload.getClass()));
+//            }
+//        }
+//    }
 }
 
 
